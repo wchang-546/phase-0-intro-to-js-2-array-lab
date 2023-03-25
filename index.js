a90cbb3a18ba8732768744213733cb28e0901b93
Will Chang <william.chaang@gmail.com>
2023-03-25 12:03:07 -0600

    Lab work done

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"] 
+
+function destructivelyAppendCat(name){
+    cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat(){
+    cats.shift();
+}
+
+function appendCat(name){
+  return [...cats,"Broom"];
+}
+
+function prependCat(name){
+    return ["Arnold",...cats];
+}
+
+function removeLastCat(){
+   return cats.slice(0,2);
+}
+
+function removeFirstCat(){
+    return cats.slice(1);
+}
